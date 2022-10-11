c07260652031ba956326ec9f502bae1fa459cafa
iamrosada <luisrosada2018@gmail.com>
2022-10-11 20:07:57 +0300

    feat: updated the navbar

--- PROJECT_CLEAN_CODE_1/src/components/Navbar/Navbar.tsx
+++ PROJECT_CLEAN_CODE_1/src/components/Navbar/Navbar.tsx
@@ -1,16 +1,42 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
 import React from "react";
+import { CustomDialog } from "../CustomDialog";
+import { FavoriteTable } from "./FavoriteTable";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { dialogOpenSubject$ } from "../CustomDialog/CustomDialog";
+import { useSelector } from "react-redux";
+import { AppStore } from "@/redux/store";
+
 export interface NavbarInterface {}
 
 const Navbar: React.FC<NavbarInterface> = () => {
+  const stateFavorites = useSelector((store: AppStore) => store.favorites);
+
+  const handleClick = () => {
+    dialogOpenSubject$.setSubject = true;
+  };
+
   return (
-    <AppBar position="fixed">
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Programming React Clean Code
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <>
+      <CustomDialog>
+        <FavoriteTable />
+      </CustomDialog>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Programming React With Clean Code
+          </Typography>
+          <IconButton
+            color="secondary"
+            aria-label="favorites"
+            component="label"
+            onClick={handleClick}
+          >
+            <FavoriteIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
 
