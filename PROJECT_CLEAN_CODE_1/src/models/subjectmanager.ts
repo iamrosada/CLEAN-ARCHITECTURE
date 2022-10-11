25d91deac4e23327a2544af0b708d60707916872
iamrosada <luisrosada2018@gmail.com>
2022-10-11 20:10:15 +0300

    feat: created the class for manager the modal

---
+++ PROJECT_CLEAN_CODE_1/src/models/subjectmanager.ts
@@ -0,0 +1,14 @@
+import { Subject } from 'rxjs'
+
+export class SubjectManager<T>{
+  private subject = new Subject<T>()
+
+  get getSubject() {
+    return this.subject.asObservable()
+  }
+
+  set setSubject(value: T) {
+    this.subject.next(value)
+  }
+
+}
